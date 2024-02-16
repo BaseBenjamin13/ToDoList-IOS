5e45ecfe5b0bb7250a649f2353ddc379ba0d9a95
BaseBenjamin13 <benmorgiewicz@gmail.com>
2024-02-15 20:59:54 -0500

    add login HeaderView

--- ToDoList/ToDoList/Views/LoginView.swift
+++ ToDoList/ToDoList/Views/LoginView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text("Login")
+        VStack {
+            // header
+            HeaderView()
+            
+            // login
+            
+            Spacer()
+        }
     }
 }
 
--- ToDoList/ToDoList/Views/MainView.swift
+++ ToDoList/ToDoList/Views/MainView.swift
@@ -14,13 +14,9 @@ struct MainView: View {
     }
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Main")
+        NavigationView {
+            LoginView()
         }
-        .padding()
     }
 }
 
