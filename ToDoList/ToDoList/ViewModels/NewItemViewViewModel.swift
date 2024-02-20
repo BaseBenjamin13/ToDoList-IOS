a2f1632a0dc70f98405151624529e0fe2cdecdc4
BaseBenjamin13 <benmorgiewicz@gmail.com>
2024-02-20 10:37:52 -0500

    add save newToDo to firebase

--- ToDoList/ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     
@@ -15,9 +17,34 @@ class NewItemViewViewModel: ObservableObject {
     
     init() {}
     
+    
     func save() {
+        guard canSave else {
+            print("hit canSave guard in save func")
+            return
+        }
+        guard let uId = Auth.auth().currentUser?.uid else {
+            print("no uId for currentUser / user not logged in")
+            return
+        }
         
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
+    
     
     var canSave: Bool {
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
