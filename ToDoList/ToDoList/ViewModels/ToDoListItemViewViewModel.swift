6a77e4ff2db20044140e448a832f44f8b474ca65
BaseBenjamin13 <benmorgiewicz@gmail.com>
2024-02-21 23:06:27 -0500

    add display for profile view

--- ToDoList/ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ToDoListItemViewViewModel: ObservableObject {
     init() {
@@ -13,6 +15,19 @@ class ToDoListItemViewViewModel: ObservableObject {
     }
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            print("User appears to not be logged in.")
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
--- ToDoList/ToDoList/Views/ProfileView.swift
+++ ToDoList/ToDoList/Views/ProfileView.swift
@@ -8,12 +8,62 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @StateObject var viewmodel = ProfileViewViewModel()
+    @StateObject var viewModel = ProfileViewViewModel()
     
     var body: some View {
         NavigationView {
             VStack {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    .frame(width: 125, height: 125)
+                    .padding()
                 
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Name: ")
+                            .bold()
+                        Text("Benji")
+                    }
+                    .padding(5)
+                    HStack {
+                        Text("Email: ")
+                            .bold()
+                        Text("BenmoirgiaF")
+                    }
+                    .padding(5)
+                    HStack {
+                        Text("Member Since: ")
+                            .bold()
+                        Text("1776")
+                    }
+                    .padding(5)
+                }
+                .padding()
+                
+                Spacer()
+                Button("Log Out") {
+                    viewModel.logOut()
+                }
+                .tint(.red)
+                .padding(60)                
+//                Button {
+//                    viewModel.logOut()
+//                } label: {
+//                    ZStack {
+//                        RoundedRectangle(cornerRadius: 10)
+//                            .foregroundColor(.black)
+//                        
+//                        Text("Log Out")
+//                            .foregroundColor(.red)
+//                            .bold()
+//                    }
+//                    .frame(width: 150, height: 100)
+//                }
+////                .foregroundColor(.blue)
+//                .padding()
+
             }
             .navigationTitle("Profile")
         }
